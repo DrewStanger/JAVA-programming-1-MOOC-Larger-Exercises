a2649a490ed87f53c9ad7def53f84a845470491e
Drew <drewstanger95@gmail.com>
2021-02-02 18:25:32 +0000

    Update GradeDistribution.java

--- src/main/java/GradeDistribution.java
+++ src/main/java/GradeDistribution.java
@@ -1,16 +1,6 @@
 
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author skate
- */
 public class GradeDistribution {
     private ArrayList<Integer> scores;
     private ArrayList<Integer> grades;
